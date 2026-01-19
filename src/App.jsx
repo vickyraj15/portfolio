7a67574a4217afa9a9d9f7b14fd07c9c001640d2
Vicky Raj <vickyraj707025@gmail.com>
2026-01-19 23:58:23 +0530

    fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Home />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
